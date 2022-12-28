0793e75fd7274b33ec91cc1e68e1dfb343685e1a
Enoc Delgado <enoc.delgado.technology@gmail.com>
2022-12-27 22:43:15 -0500

    Google SignIn

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebServer + RESTServer
+# WebServer + RESTServer + JWT + GOOGLE IDENTITY
 
-This is the RESTServer project of Fernando Herrera's Node.js course. It uses MongoDB to create a CRUD and json web token
+This is the RESTServer project of Fernando Herrera's Node.js course. It uses MongoDB to create a CRUD with json web token and google identity to authenticate.
 
 ## Installation
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express")
 const bcrypt = require("bcrypt")
 const User = require("../models/user")
 const { generateJWT } = require("../helpers/generate-jwt")
+const { googleVerify } = require("../helpers/google-verify")
 
 const login = async( req, res = response ) => {
 
@@ -45,8 +46,58 @@ const login = async( req, res = response ) => {
             message: "Talk with admin"
         })
     }
+}
 
+const googleSignIn = async( req, res = response) => {
+
+    const { id_token } = req.body
+
+    try {
+        const { email, name, picture } = await googleVerify( id_token )
+
+        // Verify if user not exist
+        let user = await User.findOne({ email })
+
+        if ( !user ) {
+            // Have to create it
+            const data = {
+                name,
+                email,
+                password: "pm9",
+                picture,
+                role: "USER_ROLE",
+                google: true
+            }
+
+            user = new User( data )
+            await user.save()
+        }
+
+        // Verify if user has false status
+        if (!user.state ) {
+            return res.status(400).json({
+                msg: "Talk to the administrator, user with blocked access"
+            })
+        }
+
+        // Generate jwt token
+        const token = await generateJWT( user.id )
+        
+        res.json({
+            user,
+            token
+        })
+    } catch ( err ) {
+        res.status( 400 ).json({
+            ok: false,
+            msg: "Token not verified"
+        })
+    }
 
 }
 
-module.exports = login
+
+module.exports = { 
+    login,
+    googleSignIn
+}
